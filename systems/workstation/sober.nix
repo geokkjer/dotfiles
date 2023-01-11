7cc0cbeb698d2e4dcf6706f20cec07465bcd5852
Geir Okkenhaug Jerstad <geokkjer@gmail.com>
2023-01-11 18:56:42 +0100

    started config for workstation in the-sober-counsel.org

---
+++ systems/workstation/sober.nix
@@ -0,0 +1,13 @@
+#
+# This file is auto-generated from "the-sober-counsel.org"
+#
+{ config, pkgs, ... }:
+{
+  imports = [
+    {
+      networking.hostName = "the-sober-counsel";
+    
+      system.stateVersion = "22.05";
+    }
+  ];
+}
