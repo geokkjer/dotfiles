ee9604138d3b0ea8e17fe0f375267dc3529452b0
Geir Okkenhaug Jerstad <geokkjer@gmail.com>
2023-03-18 00:16:27 +0100

    more logical ordering

--- systems/laptop/configuration.nix
+++ systems/laptop/configuration.nix
@@ -3,7 +3,8 @@
 {
   imports =
     [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
+      ./hardware-configuration.nixi
+      ../users/geir.nix
     ];
 
   # Bootloader.
