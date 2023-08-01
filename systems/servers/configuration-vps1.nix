5a81bebe7e9f76d6760cda2378fb18542a948120
Geir Okkenhaug Jerstad <geokkjer@gmail.com>
2023-08-01 19:17:01 +0200

    added vps config and point to point wireguard connection

---
+++ systems/servers/configuration-vps1.nix
@@ -0,0 +1,49 @@
+{ pkgs, configs, ... }:
+
+{
+  imports = [ ./gandicloud.nix ];
+
+  environment.systemPackages = with pkgs; [
+    neovim curl htop glances neofetch
+    wireguard-tools
+  ];
+
+  # Firewall 
+  networking.firewall = {
+    allowedTCPPorts = [ ];
+    allowedUDPPorts = [ 51820 ];
+  };
+  # Wireguard quick
+  networking.wg-quick.interfaces = {
+    wg0 = {
+      address = [ "192.168.100.1/24" ]; # "fdc9:281f:04d7:9ee9::1/64" 
+      listenPort = 51820;
+      privateKeyFile = "/root/wg-private";
+      peers = [
+        {
+          # laptop
+          publicKey = "G86mclNgIuxV+16AU60kwQI4nQhAbko0b5ehmXe3XAI=";
+          presharedKeyFile = "/root/wg-preshared";
+          allowedIPs = [ "192.168.100.1/24"]; # "fdc9:281f:04d7:9ee9::2/128" 
+        }
+        # More peers can be added here.
+      ];
+    };
+  };
+
+  # nginx reverse proxy
+  services.nginx = {
+    enable = true;
+    recommendedGzipSettings = true;
+    recommendedOptimisation = true;
+    recommendedProxySettings = true;
+    recommendedTlsSettings = true;
+       virtualHosts."vps1.geokkjer.eu" = { default = true; enableACME = true; addSSL = true; locations."/".proxyPass = "http://127.0.0.1:9955/"; };
+  };
+
+  # acme let's encrypt
+  security.acme = {
+    acceptTerms = true;
+    email = "geokkjer@gmail.com";
+  };
+}
---
+++ systems/servers/copy-conf-vps1.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+scp ~/dotfiles/systems/servers/configuration-vps1.nix root@46.226.104.98:/etc/nixos/configuration.nix
