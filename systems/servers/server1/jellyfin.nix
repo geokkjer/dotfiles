e46d27ba7ff2a4efd864ec68f75d913bb1c93ecc
Geir Okkenhaug Jerstad <geokkjer@gmail.com>
2023-08-06 10:40:07 +0200

    opened ports for jefflyfin

--- systems/servers/server1/jellyfin.nix
+++ systems/servers/server1/jellyfin.nix
@@ -1,4 +1,6 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 {
   services.jellyfin.enable = true;
+  networking.firewall.allowedTCPPorts = [ 8096 8920 ];
+  networking.firewall.allowedUDPPorts = [ 1900 7359 ];
 }
