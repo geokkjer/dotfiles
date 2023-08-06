4991a8b3b87a141bb01a18127c2a6b03ecfc8ed1
Geir Okkenhaug Jerstad <geokkjer@gmail.com>
2023-08-06 10:32:17 +0200

    typo

--- systems/servers/server1/podman.nix
+++ systems/servers/server1/podman.nix
@@ -3,5 +3,5 @@
   virtualisation.podman.enable = true;
   virtualisation.podman.dockerCompat = true;
   virtualisation.podman.dockerSocket.enable = true;
-  virtualisation.defaultNetwork.settings.dns_enabled = true;
+  #virtualisation.defaultNetwork.settings.dns_enabled = true;
 }
