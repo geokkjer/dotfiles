8f7368c0c3978c04c4597e542f08926055f9e73c
Geir Okkenhaug Jerstad <geokkjer@gmail.com>
2023-09-24 12:27:02 +0200

    added reverse proxy ip to nextcloud trusted domains

--- systems/servers/vps1/configuration-vps1.nix
+++ systems/servers/vps1/configuration-vps1.nix
@@ -18,27 +18,34 @@
   services.tailscale.enable = true;
 
   # nginx reverse proxy
-  services.nginx = {
-    enable = true;
-    recommendedGzipSettings = true;
-    recommendedOptimisation = true;
-    recommendedProxySettings = true;
-    recommendedTlsSettings = true;
+  let
+    Host = "vps1.tail807ea.ts.net"
+  in
+    services.nginx = {
+      enable = true;
+      recommendedGzipSettings = true;
+      recommendedOptimisation = true;
+      recommendedProxySettings = true;
+      recommendedTlsSettings = true;
 
-       virtualHosts."cloud.geokkjer.eu" = {
-         default = false;
-         enableACME = true;
-         addSSL = true;
-         locations."/".proxyPass = "http://server1.tail807ea.ts.net".proxy_set_header = "vps1.tail807ea.ts.net";
-       };
-       #virtualHosts."geokkjer.eu" = {
-       #  default = true;
-       #  forceSSL = true;
-       #  enableACME = true;
-       #  locations."/".proxyPass = "http://127.0.0.1/";
-       #};
-  };
-  # acme let's encrypt
+      virtualHosts = {
+        "cloud.geokkjer.eu" = {
+          default = false;
+          enableACME = true;
+          addSSL = true;
+        locations."/" = {
+          proxyPass = "http://server1.tail807ea.ts.net";
+        };
+        };
+        #virtualHosts."geokkjer.eu" = {
+      #  default = true;
+        #  forceSSL = true;
+        #  enableACME = true;
+        #  locations."/".proxyPass = "http://127.0.0.1/";
+      #};
+      };
+    };
+      # acme let's encrypt
   security.acme = {
     acceptTerms = true;
     email = "geokkjer@gmail.com";
