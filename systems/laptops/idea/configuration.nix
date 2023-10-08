8a89695ba55f35b99c0f0347c4837b94b6519ee9
Geir Okkenhaug Jerstad <geokkjer@gmail.com>
2023-10-08 11:50:59 +0200

    added gdm removed plasma 5 from ideapad

--- systems/laptops/idea/configuration.nix
+++ systems/laptops/idea/configuration.nix
@@ -48,8 +48,8 @@
   services.xserver.videoDrivers = [ "amdgpu" ];
 
   # Enable the KDE Plasma Desktop Environment.
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = false;
 
   # Configure keymap in X11
   services.xserver = {
