6515aca2a58e3aec9f9be02a53c69df2e6ea7239
Geir Okkenhaug Jerstad <geokkjer@gmail.com>
2023-10-08 12:27:54 +0200

    added gnome to ideapad

--- systems/laptops/idea/configuration.nix
+++ systems/laptops/idea/configuration.nix
@@ -49,7 +49,7 @@
 
   # Enable the KDE Plasma Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = false;
+  services.xserver.desktopManager.gnome.enable = true;
 
   # Configure keymap in X11
   services.xserver = {
