f0fb47d182e00b97347ade079fc84d34c6a01fad
Geir Okkenhaug Jerstad <geokkjer@gmail.com>
2023-10-09 21:07:12 +0200

    audiobookshelf test

--- systems/servers/server1/audiobook.nix
+++ systems/servers/server1/audiobook.nix
@@ -3,8 +3,8 @@
   environment.systemPackages = [
     pkgs.audiobookshelf
   ];
-  services.audiobookshelf.enable = true;
-  services.audiobookshelf.host = "0.0.0.0" ;
-  services.audiobookshelf.port = 8000;
-  services.audiobookshelf.openFirewall = true;
+  #services.audiobookshelf.enable = true;
+  #services.audiobookshelf.host = 0.0.0.0 ;
+  #services.audiobookshelf.port = 8000;
+  #services.audiobookshelf.openFirewall = true;
 }
